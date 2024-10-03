558ee3c2dc1b3e29b656e2c93b8e54aca59721a1
sumo456 <danielsalvadortarraga@gmail.com>
2024-10-03 19:34:56 +0200

    a

--- Java/Condicionales/M3_UF1_A02_03.java
+++ Java/Condicionales/M3_UF1_A02_03.java
@@ -1,5 +1,13 @@
 import java.util.Scanner;
 
 public class M3_UF1_A02_03 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce un numero: ");
+        int num = sc.nextInt();
+        
+        if (num >0 && num < 100) {
+            System.out.println("El numero introducido es menor a 100 y positivo");
+        }
+    }
 }
