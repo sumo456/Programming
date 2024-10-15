834d12fa276da32211472405f11840c2b3da8cc5
sumo456 <danielsalvadortarraga@gmail.com>
2024-10-15 20:29:58 +0200

    a

---
+++ Java/Repaso/repaso.java
@@ -0,0 +1,16 @@
+package Programming.Java.Repaso;
+
+import java.util.Scanner;
+
+/**
+*
+* @author DanielSalvador
+*/
+
+public class repaso {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Quanta vida tens? ");
+        
+    }
+}
