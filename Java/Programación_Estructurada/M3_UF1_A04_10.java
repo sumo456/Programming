8af591c0b741c9c53d417972d0c32bf82c7b64e1
sumo456 <danielsalvadortarraga@gmail.com>
2024-10-23 17:53:41 +0200

    a

--- Java/Programación_Estructurada/M3_UF1_A04_10.java
+++ Java/Programación_Estructurada/M3_UF1_A04_10.java
@@ -3,6 +3,6 @@ package Programming.Java.Programación_Estructurada;
 public class M3_UF1_A04_10 {
     public static void main(String[] args) {
         int i = 0;
-        int a = 0;
+        int b = 0;
     }
 }
