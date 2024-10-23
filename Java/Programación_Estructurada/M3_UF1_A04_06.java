75b50591056bb4eb346c431784da6eb7bcf7e57c
sumo456 <danielsalvadortarraga@gmail.com>
2024-10-23 17:29:38 +0200

    s

--- Java/Programación_Estructurada/M3_UF1_A04_06.java
+++ Java/Programación_Estructurada/M3_UF1_A04_06.java
@@ -10,7 +10,7 @@ public class M3_UF1_A04_06 {
         
         for (int i = 0; i < matriz.length; i++){
             System.out.println("Entra un numero: ");
-            matriz[1] = sc.nextInt();
+            matriz[i] = sc.nextInt();
         }
         System.out.println("Els valor introduits són: ");
 
