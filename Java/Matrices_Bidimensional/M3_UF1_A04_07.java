f87c994beea8c9adfe377cb1b3ab88f55ce42077
sumo456 <danielsalvadortarraga@gmail.com>
2024-10-29 19:52:38 +0100

    a

---
+++ Java/Matrices_Bidimensional/M3_UF1_A04_07.java
@@ -0,0 +1,19 @@
+package Programming.Java.Matrices_Bidimensional;
+
+import java.util.Scanner;
+
+public class M3_UF1_A04_07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int matriz1 [][] = new int [3][3];
+        int matriz2 [][] = new int [3][3];
+
+        for (int i = 0; i < matriz1.length; i++){
+            for (int j = 0; j < matriz1.length; j++){
+                
+            }
+        }
+
+    }
+}
