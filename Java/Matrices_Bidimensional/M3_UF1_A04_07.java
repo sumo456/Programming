aea4a1b7b1bc517f60d55b2657295e6148e045a7
sumo456 <danielsalvadortarraga@gmail.com>
2024-10-29 20:11:03 +0100

    a

--- Java/Matrices_Bidimensional/M3_UF1_A04_07.java
+++ Java/Matrices_Bidimensional/M3_UF1_A04_07.java
@@ -8,12 +8,32 @@ public class M3_UF1_A04_07 {
 
         int matriz1 [][] = new int [3][3];
         int matriz2 [][] = new int [3][3];
+        int sumamatrizes [][] = new int [3][3];
 
+        //Añadir valores  a matriz1
         for (int i = 0; i < matriz1.length; i++){
             for (int j = 0; j < matriz1.length; j++){
-                
+                System.out.print("Entra un valor: ");
+                matriz1[i][j] = sc.nextInt();
             }
         }
+
+        //Añadir valores  a matriz2
+        for (int i = 0; i < matriz2.length; i++){
+            for(int j = 0; j < matriz2.length; j++){
+                System.out.print("Entra un valor: ");
+                matriz2[i][j] = sc.nextInt();
+            }
+        }
+
+        //Sumar matrices
+        for (int i = 0; i < sumamatrizes.length; i++){
+            for (int j = 0; j < sumamatrizes.length; j++){
+                sumamatrizes[i][j] = matriz1[i][j] + matriz2[i][j];
+                System.out.println(sumamatrizes[i][j] + "\t");
+            }
+        }
+
 
     }
 }
