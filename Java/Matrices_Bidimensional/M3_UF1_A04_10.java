0ccaeb777f5828ea2784a33d0abe747b310da762
sumo456 <danielsalvadortarraga@gmail.com>
2024-10-30 17:52:17 +0100

    a

--- Java/Matrices_Bidimensional/M3_UF1_A04_10.java
+++ Java/Matrices_Bidimensional/M3_UF1_A04_10.java
@@ -6,6 +6,16 @@ import java.util.Random;
 public class M3_UF1_A04_10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        int matriz[][] = new int [0][0];
+
+
+
         
+        for(int i = 0; i<matriz.length; i++){
+            for(int j = 0; j<matriz.length; j++){
+
+            }
+        }
     }
 }
