ee213e051bd176ea3e410a6d9fc30b53567c1955
sumo456 <danielsalvadortarraga@gmail.com>
2024-11-06 16:40:46 +0100

    a

--- Java/Matrices_Bidimensional/M3_UF1_A04_10.java
+++ Java/Matrices_Bidimensional/M3_UF1_A04_10.java
@@ -26,5 +26,49 @@ public class M3_UF1_A04_10 {
                 }
             }
         }
+
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Introdueix el nombre de files (N): ");
+        int files = sc.nextInt();
+        System.out.print("Introdueix el nombre de columnes (M): ");
+        int columnes = sc.nextInt();
+
+        int[][] matriu = new int[files][columnes];
+        
+        System.out.println("Introdueix els elements de la matriu:");
+        int i = 0;
+        while (i < files) {
+            int j = 0;
+            while (j < columnes) {
+                System.out.print("Element [" + i + "][" + j + "]: ");
+                matriu[i][j] = sc.nextInt();
+                j++;
+            }
+            i++;
+        }
+
+        // Comprovació si la matriu és nul·la
+        boolean esNul·la = true;
+        i = 0;
+        while (i < files && esNul·la) {
+            int j = 0;
+            while (j < columnes) {
+                if (matriu[i][j] != 0) {
+                    esNul·la = false;
+                    break;
+                }
+                j++;
+            }
+            i++;
+        }
+
+        if (esNul·la) {
+            System.out.println("La matriu és nul·la.");
+        } else {
+            System.out.println("La matriu no és nul·la.");
+        }
     }
 }
