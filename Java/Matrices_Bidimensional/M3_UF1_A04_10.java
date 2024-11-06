feabc18ffdd0af387f48777c377cca8922938a87
sumo456 <danielsalvadortarraga@gmail.com>
2024-11-06 16:21:39 +0100

    a

--- Java/Matrices_Bidimensional/M3_UF1_A04_10.java
+++ Java/Matrices_Bidimensional/M3_UF1_A04_10.java
@@ -1,20 +1,29 @@
 package Programming.Java.Matrices_Bidimensional;
 
 import java.util.Scanner;
-import java.util.Random;
 
 public class M3_UF1_A04_10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int matriz[][] = new int [0][0];
-
+        System.out.println("Numero de filas: ");
+        int filas = sc.nextInt();
+        System.out.println("Numero de columnas: ");
+        int columnas = sc.nextInt();
+        int matriz[][] = new int [filas][columnas];
 
 
         
         for(int i = 0; i<matriz.length; i++){
+            if(i == 0){
+                System.out.println("La matriz es nula");
+                break;
+            }
             for(int j = 0; j<matriz.length; j++){
-
+                if(j == 0){
+                    System.out.println("La matriz es nula");
+                    break;
+                }
             }
         }
     }
